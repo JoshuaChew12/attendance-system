c4cc775b6752fe6b1c875d786e36097f655fa4dd
JoshuaChew12 <jayz.chew.one@gmail.com>
2026-06-29 08:36:55 +0800

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,41 @@
+async function apiPost(data){
+
+const response =
+await fetch(API_URL,{
+method:"POST",
+
+headers:{
+"Content-Type":
+"application/json"
+},
+
+body:
+JSON.stringify(data)
+
+});
+
+
+return await response.json();
+
+}
+
+
+
+async function apiGet(params){
+
+
+const query =
+new URLSearchParams(params);
+
+
+
+const response =
+await fetch(
+API_URL+"?"+query
+);
+
+
+return await response.json();
+
+
+}
