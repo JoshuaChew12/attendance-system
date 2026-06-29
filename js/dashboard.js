e3c9d73eed5f8c5305d28c72c11a864bf42d4563
JoshuaChew12 <jayz.chew.one@gmail.com>
2026-06-29 15:34:34 +0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,6 +1,9 @@
 async function loadDashboard(){
 
 
+try{
+
+
 const user =
 JSON.parse(
 localStorage.getItem("user")
@@ -8,17 +11,32 @@ localStorage.getItem("user")
 
 
 
+if(!user){
+
+window.location.href="index.html";
+
+return;
+
+}
+
+
+
 const result =
 await apiGet({
 
-action:
-"getDashboard",
+action:"getDashboard",
 
-branch:
-user.branch_id
+branch:user.branch_id
 
 });
 
+
+
+console.log(result);
+
+
+
+if(result.success){
 
 
 document
@@ -33,6 +51,55 @@ document
 .innerHTML =
 result.data.late;
 
+
+}else{
+
+
+document
+.getElementById("msg")
+.innerHTML =
+result.message;
+
+
+}
+
+
+
+}catch(error){
+
+
+console.error(error);
+
+
+document
+.getElementById("msg")
+.innerHTML =
+error.message;
+
+
+}
+
+
+
+}
+
+
+
+function goCheck(){
+
+window.location.href=
+"checkin.html";
+
+}
+
+
+
+function logout(){
+
+localStorage.removeItem("user");
+
+window.location.href=
+"index.html";
 
 }
 
