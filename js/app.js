a44659c3e527752a3ff0083b1df1927d31e42084
JoshuaChew12 <jayz.chew.one@gmail.com>
2026-06-30 15:35:53 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,161 +3,99 @@
 Attendance App Router
 ==========================
 */
-
-
 function loadPage(page){
-
 
 fetch(
 "pages/"+page+".html"
 )
 
-
 .then(
 response=>response.text()
 )
 
-
 .then(
 html=>{
-
 
 const content =
 document.getElementById(
 "content"
 );
 
-
-
 content.innerHTML =
 html;
-
-
 
 content.classList.remove(
 "page-animation"
 );
 
-
-
 void content.offsetWidth;
-
-
 
 content.classList.add(
 "page-animation"
 );
 
-
-
-
 loadScript(page);
-
-
 
 });
 
-
 }
 
+const PageController = {
 
+home: loadHome,
+scan: initScanPage,
+calendar: initCalendarPage
 
-
+};
 
 function loadScript(page){
 
+const old=document.getElementById("page-script");
+if(old) old.remove();
 
+const script=document.createElement("script");
+script.id="page-script";
+script.src="js/"+page+".js";
 
-const old =
-document.getElementById(
-"page-script"
-);
+script.onload=function(){
 
-
-
-if(old){
-
-old.remove();
-
+if(PageController[page]){
+PageController[page]();
 }
 
+};
 
-
-const script =
-document.createElement(
-"script"
-);
-
-
-
-script.id =
-"page-script";
-
-
-
-script.src =
-"js/"+page+".js";
-
-
-
-document.body.appendChild(
-script
-);
-
-
+document.body.appendChild(script);
 
 }
-
-
-
-
-
-
 
 // Login Check
-
-
 function checkLogin(){
-
 
 const user =
 localStorage.getItem(
 "user"
 );
 
-
-
 if(!user){
-
 
 window.location.href=
 "index.html";
 
-
 return false;
 
-
 }
-
 
 return true;
 
-
 }
 
 
-
-
-
 // Start App
-
-
 if(checkLogin()){
-
 
 loadPage(
 "home"
 );
-
 
 }
