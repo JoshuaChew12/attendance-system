6e2793ad0805c78d2d983de202679db10dc6a83c
JoshuaChew12 <jayz.chew.one@gmail.com>
2026-06-30 16:01:54 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,106 +1,163 @@
-const PageController = {
+/*
+==========================
+Attendance App Router
+==========================
+*/
 
-home: loadHome,
-scan: initScanPage,
-calendar: initCalendarPage
-
-};
 
 function loadPage(page){
+
 
 fetch(
 "pages/"+page+".html"
 )
 
+
 .then(
 response=>response.text()
 )
 
+
 .then(
 html=>{
+
 
 const content =
 document.getElementById(
 "content"
 );
 
+
+
 content.innerHTML =
 html;
+
+
 
 content.classList.remove(
 "page-animation"
 );
 
+
+
 void content.offsetWidth;
+
+
 
 content.classList.add(
 "page-animation"
 );
 
+
+
+
 loadScript(page);
+
+
 
 });
 
+
 }
+
+
+
+
 
 function loadScript(page){
 
-const old=document.getElementById("page-script");
-if(old) old.remove();
 
-const script=document.createElement("script");
-script.id="page-script";
-script.src="js/"+page+".js";
 
-script.onload=function(){
+const old =
+document.getElementById(
+"page-script"
+);
 
-console.log("PAGE LOADED:", page);
 
-// ===============================
-// SAFE CALL CONTROLLER
-// ===============================
 
-const fn = PageController[page];
+if(old){
 
-if(typeof fn === "function"){
-fn();
-}else{
-console.warn("No controller for page:", page);
-}
-
-};
-
-document.body.appendChild(script);
+old.remove();
 
 }
+
+
+
+const script =
+document.createElement(
+"script"
+);
+
+
+
+script.id =
+"page-script";
+
+
+
+script.src =
+"js/"+page+".js";
+
+
+
+document.body.appendChild(
+script
+);
+
+
+
+}
+
+
+
+
+
+
 
 // Login Check
+
+
 function checkLogin(){
+
 
 const user =
 localStorage.getItem(
 "user"
 );
 
+
+
 if(!user){
+
 
 window.location.href=
 "index.html";
 
+
 return false;
 
+
 }
+
 
 return true;
 
+
 }
 
 
+
+
+
 // Start App
+
+
 if(checkLogin()){
+
 
 loadPage(
 "home"
 );
+
 
 }
